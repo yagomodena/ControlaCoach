153017f491d5b3e0c9b921c618c44ef24c7d3bd0
Yago Modena <yagobmodena1@gmail.com>
2025-06-20 12:11:05 +0000

    Conectando ao firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALAhGiJs462b3CKJ68C7w_dQvOYfviI6Y",
+  authDomain: "bossolan-futevlei-manager.firebaseapp.com",
+  projectId: "bossolan-futevlei-manager",
+  storageBucket: "bossolan-futevlei-manager.firebasestorage.app",
+  messagingSenderId: "439422165067",
+  appId: "1:439422165067:web:69a744e4234cd32469adce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
