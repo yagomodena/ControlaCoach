321d3a0fcb91b0506a75d0b32708b8330a09f8ed
Yago Modena <yagobmodena1@gmail.com>
2025-09-03 20:21:40 +0000

    ajustes para publicar

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,6 +1,8 @@
 
 'use client';
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
